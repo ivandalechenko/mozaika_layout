bbb8f38fb522e9cd3c8b034e8b59f528f5db243a
Иван Далеченко <ivandalechenko@gmail.com>
2023-06-04 15:53:37 +0300

    beauty mockup

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn_chose_pick').onclick = function () {
+    // document.getElementById('btn_chose_pick').classList.add('anim')
+    document.getElementById('cont').classList.add('cont_anim')
+    document.getElementById('demo').classList.add('hide_to_up_anim')
+    document.getElementById('btn_chose_pick').classList.add('hide_to_down_anim')
+
+}
